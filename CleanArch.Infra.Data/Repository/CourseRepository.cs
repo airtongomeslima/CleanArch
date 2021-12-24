48dcb4487702bfbe1aea4345d6f2f8900b408f21
airto <airto@DESKTOP-C69MOQV>
2021-12-24 12:33:00 -0300

    Add project files.

---
+++ CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _universityDBContext;
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            _universityDBContext = universityDBContext;
+        }
+
+        public List<Course> GetCourses()
+        {
+            return _universityDBContext.Courses.ToList();
+        }
+    }
+}
